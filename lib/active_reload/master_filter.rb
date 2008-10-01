4aaa0298207ec5042f66afcc68ccb69ae4e5b48f
Andrew Bloom <andrew@andrewkbloom.com>
2008-10-01 14:20:09 -0500

    added some documentation

--- lib/active_reload/master_filter.rb
+++ lib/active_reload/master_filter.rb
@@ -1,9 +1,10 @@
 module ActiveReload
+  # MasterFilter should be used as an around filter in your controllers that require certain actions to use the Master DB for reads as well as writes
   class MasterFilter
     def self.filter(controller, &block)
       proxy = ActiveRecord::Base.active_connections['ActiveRecord::Base']
 
-      if proxy and proxy.respond_to? :with_master
+      if proxy and proxy.respond_to?(:with_master)
         proxy.with_master(&block)
       else
         yield block

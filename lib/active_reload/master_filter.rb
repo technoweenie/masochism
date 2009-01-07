76165106bc0d1e9bd2caac6f033e14e6adbc099b
Andrew Bloom <andrew@andrewkbloom.com>
2009-01-08 06:07:53 +0800

    updating master filter to work with new masochism updates
    Signed-off-by: rick <technoweenie@gmail.com>

--- lib/active_reload/master_filter.rb
+++ lib/active_reload/master_filter.rb
@@ -2,10 +2,8 @@ module ActiveReload
   # MasterFilter should be used as an around filter in your controllers that require certain actions to use the Master DB for reads as well as writes
   class MasterFilter
     def self.filter(controller, &block)
-      proxy = ActiveRecord::Base.active_connections['ActiveRecord::Base']
-
-      if proxy.respond_to?(:with_master)
-        proxy.with_master(&block)
+      if ActiveRecord::Base.connection.respond_to?(:with_master)
+        ActiveRecord::Base.connection.with_master(&block)
       else
         yield block
       end
